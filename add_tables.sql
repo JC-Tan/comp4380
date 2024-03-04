5ed1ae8cb30a67bb826298b5c839e5ae8f2a1ecf
JC-Tan <93283233+JC-Tan@users.noreply.github.com>
2024-03-03 19:20:16 -0600

    more cleaning

---
+++ add_tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE daily_passenger_activity (
+	schedule_period_name		varchar(20),
+	schedule_period_start_date	timestamp,
+	schedule_period_end_date	timestamp,
+	stop_number					integer,
+	route_number				integer,
+	route_name					varchar(255),
+	day_type					varchar(10),
+	time_period					varchar(10),
+	average_boardings			decimal,
+	average_alightings			decimal,
+	location					varchar(100),
+	PRIMARY KEY(
+		schedule_period_name,
+		schedule_period_start_date,
+		schedule_period_end_date,
+		stop_number,
+		route_number,
+		day_type,
+		time_period
+	)
+);
+
+--Pass-Up ID,Pass-Up Type,Time,Route Number,Route Name,Route Destination,Location
+CREATE TABLE transit_pass_up (
+	pass_up_id					integer PRIMARY KEY NOT NULL,
+	pass_up_type				varchar(50),
+	pass_up_time				timestamp,
+	route_number				integer,
+	route_name					varchar(255),
+	route_destination			varchar(255),
+	location					varchar(100),
+);
+
+COPY public.daily_passenger_activity FROM '{PATH_TO}\dailyPassengerActivity.txt' (FORMAT CSV, DELIMITER(','));
--- main.py
+++ main.py
@@ -7,6 +7,21 @@ def atoi(text):
 def natural_sort(text):
     return [atoi(c) for c in re.split(r'(\d+)', text)]
 
+def replace(line):
+    bus_dict = {"/": "-", "BLUE": "1000", "S401" : "1401", "S402" : "1402", "S404" : "1404",
+        "S405" : "1405", "S406" : "1406", "S408" : "1408", "S409" : "1409", "S410" : "1410",
+        "S412" : "1412", "S413" : "1413", "S414" : "1414", "S416" : "1416", "S417" : "1417",
+        "S418" : "1418", "S419" : "1419", "S420" : "1420", "S421" : "1421", "S422" : "1422", 
+        "S425" : "1425", "S426" : "1426", "S427" : "1427", "S428" : "1428", "S429" : "1429",
+        "S430" : "1430", "S431" : "1431", "S432" : "1432", "S433" : "1433", "S435" : "1435",
+        "S436" : "1436", "S437" : "1437", "S438" : "1438", "S439" : "1439", "S440" : "1440",
+        "S441" : "1441", "S442" : "1442", "S443" : "1443", "W203" : "1203", "W205" : "1205",
+        "W240" : "1240", "I310" : "1310", "I309" : "1309", "S434" : "1434",
+        }
+    bus_dict = dict((re.escape(k), v) for k, v in bus_dict.items())
+    pattern = re.compile("|".join(bus_dict.keys()))
+    return pattern.sub(lambda m: bus_dict[re.escape(m.group(0))], line)
+
 def split(filename, output_name):
     NUM_LINES = 150000
     dir_name = f'{output_name}'
@@ -17,7 +32,7 @@ def split(filename, output_name):
         outFile = open(f'{output_name}\\{output_name}_{file_count}.txt', 'w')
         count = 0
         for line in inFile:
-            outFile.write(line.replace('/', '-').replace('BLUE', '1000'))
+            outFile.write(replace(line))
             count -= -1
             if count >= NUM_LINES:
                 outFile.close()
@@ -53,4 +68,5 @@ def main():
         dir_name = input("Enter the directory name: ")
         merge(dir_name)
     print('Done.')
+
 main()
